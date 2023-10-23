c3be7c4294cbe342ea98969ddafe06cc1582738d
Dima Kruglov <kruglov.dv@phystech.edu>
2023-10-23 21:45:31 +0300

    firestorage init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage, ref, uploadBytes } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private storage: Storage) {}
+
   title = 'storage';
+
+  sendData(event: any) {
+    const file = event.target.files[0];
+    const imageRef = ref(this.storage, `images/${file.name}`);
+
+    uploadBytes(imageRef, file).then(x => {
+      console.log(x);
+    }).catch(error => console.log(error));
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'storage-b8c7b',
+    appId: '1:657934740371:web:8f2f4a97f3fdf6543c9666',
+    storageBucket: 'storage-b8c7b.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBOnWriXZX6UGo6m5lp7tlInHqpf-H-sxM',
+    authDomain: 'storage-b8c7b.firebaseapp.com',
+    messagingSenderId: '657934740371',
+  },
+};
